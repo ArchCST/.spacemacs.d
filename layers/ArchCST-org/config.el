ad125665cecd24ce160386f5f3b3a8d52a1c0c4e
BruceCST <BruceCST@outlook.com>
2018-07-17 07:00:46 +0800

    create ArchCST-org layer

---
+++ layers/ArchCST-org/config.el
@@ -0,0 +1,10 @@
+(setq org-tag-alist '(
+
+                      (:startgroup . nil)
+                      ("桌面" . ?d) ("服务器" . ?s)
+                      (:endgroup . nil)
+                      ("编辑器" . ?e)
+                      ("浏览器" . ?f)
+                      ("多媒体" . ?m)
+                      ("test" . ?t)
+                      ))   
--- layers/ArchCST/config.el
+++ layers/ArchCST/config.el
@@ -1,12 +1,18 @@
+;; 在 orgmode 中实现语法高亮
+(require 'org)
+(setq org-src-fontify-natively t)
+
+;; org 文件自动换行
+(add-hook 'org-mode-hook 'spacemacs/toggle-visual-line-navigation-on)
+
+;; org 回车打开链接
+(setq org-return-follows-link t)
+
 ;; 全局开启行号
 (global-linum-mode t)
 
 ;; 关闭启动画面
 (setq inhibit-splash-screen t)
-
-;; 在 orgmode 中实现语法高亮
-(require 'org)
-(setq org-src-fontify-natively t)
 
 ;; 设置org-agenda-files的目录
 (setq org-agenda-files '("~/Dropbox/OrgCST/"))
@@ -16,3 +22,6 @@
 
 ;; 为编程语言类的 major mode 默认启用删除多个空格
 (add-hook 'prog-mode-hook 'spacemacs/toggle-hungry-delete-on)
+
+;; 设置默认浏览器
+(setq browse-url-browser-function 'browse-url-chromium)
