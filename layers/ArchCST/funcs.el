9b1f16ec752b4f8434884caad528338174baa504
AzureB4 <brucecst@outlook.com>
2018-07-24 13:00:45 +0800

    now C-c l create links with website's title

--- layers/ArchCST/funcs.el
+++ layers/ArchCST/funcs.el
@@ -10,11 +10,12 @@
                  (concat
                   "set frontmostApplication to path to frontmost application\n"
                   "tell application \"Safari\"\n"
-	                "set theUrl to get URL of current tab of first window\n"
-	                "set theResult to (get theUrl)\n"
+                  "set theUrl to get URL of current tab of first window\n"
+                  "set theTitle to get name of current tab of first window\n"
+                  "set theResult to {get \"[[\", theUrl, \"][\", theTitle, \"]]\"}\n"
                   "end tell\n"
                   "activate application (frontmostApplication as text)\n"
                   "set links to {}\n"
                   "copy theResult to the end of links\n"
-                  "return links as string\n"))))
+                  "return links as string"))))
     (format "%s" (s-chop-suffix "\"" (s-chop-prefix "\"" result)))))
